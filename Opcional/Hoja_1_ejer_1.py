c514eaf97e53df97ce27e0f0f35f3a688c9155c1
Pederysky <145128880+Pederysky@users.noreply.github.com>
2023-09-22 14:21:06 +0200

    Ejercicios hechos en clase

---
+++ Opcional/Hoja_1_ejer_1.py
@@ -0,0 +1,5 @@
+#Realiza un programa que solicite tu nombre y apellidos y muestre por pantalla “Encantado de conocerte Pepe Sánchez López”.
+
+nombre = input("Dime tu nombre y apellidos: ")
+
+print("Encantado de conocerte", nombre)
